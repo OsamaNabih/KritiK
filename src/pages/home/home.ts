b0fdd95e4f68270c47a2c632cb394eb2a4472585
Omar Wagih <o.wagih.ow@gmail.com>
2017-12-02 13:09:51 +0200

    Project

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,51 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+RestItems: any[];
 
   constructor(public navCtrl: NavController) {
-
+    
+    this.RestItems = [
+      {
+        Restaurant: {
+          avatar: 'assets/img/das.jpg',
+          name: 'Crepelicious'
+        },
+        Address:'24 medan el mesa7a',
+        Stars: "4 stars",
+        PhoneNumber:'011554433',
+        Region:'Dokki, Giza',
+        bestReview: `This place was amazing, they serve the most delicious crepes of all time.
+        They have some REALLY AMAZING delicious stuff.`,
+      },
+      {
+        Restaurant: {
+          avatar: 'assets/img/das.jpg',
+          name: 'Crepelicious'
+        },
+        Region:'Dokki, Giza',
+        Stars: "4 stars",
+        Address:'24 medan el mesa7a',
+        PhoneNumber:'011554433',
+        bestReview: 'This place was amazing, they serve the most delicious crepes of all time.',
+      },
+      {
+        Restaurant: {
+          avatar: 'assets/img/das.jpg',
+          name: 'Crepelicious'
+        },
+        Address:'24 medan el mesa7a',
+        Stars: "4 stars",
+        PhoneNumber:'011554433',
+        Region:'Dokki, Giza',
+        bestReview: 'This place was amazing, they serve the most delicious crepes of all time.',
+      }
+    ];
   }
 
 }
