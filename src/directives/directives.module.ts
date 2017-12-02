7e27d33b7ead07e3ce522c77551e7be949429935
Omar Wagih <o.wagih.ow@gmail.com>
2017-12-02 13:11:49 +0200

    s

---
+++ src/directives/directives.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { HideHeaderDirective } from './headerscroll/headerscroll';
+@NgModule({
+	declarations: [HideHeaderDirective],
+	imports: [],
+	exports: [HideHeaderDirective]
+})
+export class DirectivesModule {}
